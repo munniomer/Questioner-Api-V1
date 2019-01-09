99efa03c50aebf020c1f96c5ddc9f1ff108a3eba
Munira <munniomer@gmail.com>
2019-01-09 12:32:17 +0300

    [finishes #163081769] adds the app files

---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+import os
+from flask import Flask
+from config import app_config
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.url_map.strict_slashes = False
+    app.config.from_object(app_config[config_name])
+    return app
