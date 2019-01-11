f6ba8fe1da6c69456e9f2033db210e29bc747b4d
Munira <munniomer@gmail.com>
2019-01-11 10:10:56 +0300

    registers meetup endpoint

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask, Blueprint
 from flask_restful import Api, Resource 
 from app.api.v1.views.users_view import SignupResource,LoginResource
-
+from app.api.v1.views.meetups_view import MeetupResource
 
 v1 = Blueprint('apiv1', __name__, url_prefix='/api/v1')
 app = Api(v1)  
@@ -11,5 +11,8 @@ app = Api(v1)
 # Users
 app.add_resource(SignupResource, '/user/register')
 app.add_resource(LoginResource, '/user/login')
+
+#Meetups
+app.add_resource(MeetupResource, '/meetups')
 
 
