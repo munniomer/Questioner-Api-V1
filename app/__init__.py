60bc33fb0be110926997306edadba810049cd2a1
Munira <munniomer@gmail.com>
2019-01-15 12:12:03 +0300

    [starts #163206246] registers error handlers

--- app/__init__.py
+++ app/__init__.py
@@ -12,6 +12,24 @@ def create_app(config_name):
     from app.api.v1 import v1
     app.register_blueprint(v1)
 
-
+    @app.errorhandler(400)
+    def handle_bad_request(error):
+        """ Handles 400 errors """
+        return 'Bad Request!', 400
+    
+    @app.errorhandler(404)
+    def handle_not_found(error):
+        """ handles 404 eroors """
+        return 'Not Found!', 404
+    
+    @app.errorhandler(403)
+    def handle_forbidden(error):
+        """Handles 403 errors"""
+        return 'Forbidden', 403
+    
+    @app.errorhandler(401)
+    def handle_unauthorized(error):
+        """Handles 401 errors"""
+        return 'Unauthorized', 403
 
     return app
