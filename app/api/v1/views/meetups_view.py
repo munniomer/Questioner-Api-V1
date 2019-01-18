2d1a5f1a72ccb927316ca7f02651654852bcdfa1
Munira <munniomer@gmail.com>
2019-01-18 11:40:49 +0300

    adds str.strip function

--- app/api/v1/views/meetups_view.py
+++ app/api/v1/views/meetups_view.py
@@ -1,5 +1,5 @@
 """User views contains Signup Resources"""
-
+import datetime
 from app.api.v1.models.meetups_model import MeetupModel
 from flask import Flask, request, jsonify
 from flask_restful import Resource
@@ -18,12 +18,14 @@ class MeetupResource(Resource):
         validate.check_data(data)
         validate.validate_meetup_data(data)
         print(data)
-        venue = data["venue"]
-        title = data["title"]
-        happening_on = data["happening_on"]
+        venue = str.strip(data["venue"])
+        title = str.strip(data["title"])
+        happening_on = str.strip(data["happening_on"])
+        validate.valid_date(happening_on)
+
        
         data = db.add_meetup(venue, title,happening_on)
-            
+        
         for meetupdata in data:
             response = { "meetup_Id": meetupdata["meetup_Id"],
                         "created_on": meetupdata["created_on"] }
